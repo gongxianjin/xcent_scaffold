6805035f37f2b0e01079d75847ad1dce75d7e6e9
617699485 <617699485@qq.com>
2021-01-07 19:24:31 +0800

    fixRouter

--- router/sys_user.go
+++ router/sys_user.go
@@ -12,7 +12,10 @@ func InitUserRouter(router *gin.RouterGroup) {
 		middleware.RequestLog(),
 		middleware.TranslationMiddleware())
 	{
-		BaseRouter.POST("/login", curd.Login)
-		BaseRouter.GET("/loginout", curd.LoginOut)
+		BaseRouter.GET("/user/listpage", curd.ListPage)
+		BaseRouter.POST("/user/add", curd.AddUser)
+		BaseRouter.POST("/user/edit", curd.EditUser)
+		BaseRouter.POST("/user/remove", curd.RemoveUser)
+		BaseRouter.POST("/user/batchremove", curd.RemoveUser)
 	}
 }
