cb2276c7bf05e7e505d00e7d8247e01007db3ed2
617699485 <617699485@qq.com>
2021-01-12 22:53:43 +0800

    addResponce

---
+++ model/response/sys_user.go
@@ -0,0 +1,13 @@
+package response
+
+import "github.com/gongxianjin/xcent_scaffold/model"
+
+type SysUserResponse struct {
+	User model.SysUser `json:"user"`
+}
+
+type LoginResponse struct {
+	User      model.SysUser `json:"user"`
+	Token     string        `json:"token"`
+	ExpiresAt int64         `json:"expiresAt"`
+}
