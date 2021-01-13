484aae5ae0d0502f8cc915fa73866ffa8a46dde6
617699485 <617699485@qq.com>
2021-01-13 10:39:00 +0800

    addUser

---
+++ controller/user.go
@@ -0,0 +1,127 @@
+package controller
+
+import ( 
+	"strings" 
+	"github.com/gin-gonic/gin" 
+	"github.com/gongxianjin/xcent-common/lib"
+	"github.com/gongxianjin/xcent_scaffold/dao"
+	"github.com/gongxianjin/xcent_scaffold/dto"
+	"github.com/gongxianjin/xcent_scaffold/middleware" 
+)
+
+type UserController struct {
+} 
+
+// @Tags SysUser
+// @Summary 分页获取用户列表
+// @Security ApiKeyAuth
+// @accept application/json
+// @Produce application/json 
+// @Param polygon body dto.ListPageInput true "body"
+// @Success 200 {string} string "{"success":true,"data":{},"msg":"获取成功"}"
+// @Router /user/ListPage [post]
+func (demo *UserController) ListPage(c *gin.Context) {
+	listInput := &dto.ListPageInput{}
+	if err := listInput.BindingValidParams(c); err != nil {
+		middleware.ResponseError(c, 2001, err)
+		return
+	}
+	if listInput.PageSize == 0 {
+		listInput.PageSize = 10
+	}
+	tx, err := lib.GetGormPool("default")
+	if err != nil {
+		middleware.ResponseError(c, 2002, err)
+		return
+	}
+	userList, total, err := (&dao.User{}).PageList(c, tx, listInput)
+	if err != nil {
+		middleware.ResponseError(c, 2003, err)
+		return
+	}
+	m := &dao.ListPageOutput{
+		List:  userList,
+		Total: total,
+	}
+	middleware.ResponseSuccess(c, m)
+	return
+}
+
+func (demo *UserController) AddUser(c *gin.Context) {
+	addInput := &dto.AddUserInput{}
+	if err := addInput.BindingValidParams(c); err != nil {
+		middleware.ResponseError(c, 2001, err)
+		return
+	}
+
+	tx, err := lib.GetGormPool("default")
+	if err != nil {
+		middleware.ResponseError(c, 2002, err)
+		return
+	}
+	user := &dao.User{
+		Name:  addInput.Name,
+		Sex:   addInput.Sex,
+		Age:   addInput.Age,
+		Birth: addInput.Birth,
+		Addr:  addInput.Addr,
+	}
+	if err := user.Save(c, tx); err != nil {
+		middleware.ResponseError(c, 2002, err)
+		return
+	}
+	middleware.ResponseSuccess(c, "")
+	return
+}
+
+func (demo *UserController) EditUser(c *gin.Context) {
+	editInput := &dto.EditUserInput{}
+	if err := editInput.BindingValidParams(c); err != nil {
+		middleware.ResponseError(c, 2001, err)
+		return
+	}
+
+	tx, err := lib.GetGormPool("default")
+	if err != nil {
+		middleware.ResponseError(c, 2002, err)
+		return
+	}
+
+	user, err := (&dao.User{}).Find(c, tx, int64(editInput.Id))
+	if err != nil {
+		middleware.ResponseError(c, 2002, err)
+		return
+	}
+
+	user.Name = editInput.Name
+	user.Sex = editInput.Sex
+	user.Age = editInput.Age
+	user.Birth = editInput.Birth
+	user.Addr = editInput.Addr
+	if err := user.Save(c, tx); err != nil {
+		middleware.ResponseError(c, 2003, err)
+		return
+	}
+	middleware.ResponseSuccess(c, "")
+	return
+}
+
+func (demo *UserController) RemoveUser(c *gin.Context) {
+	removeInput := &dto.RemoveUserInput{}
+	if err := removeInput.BindingValidParams(c); err != nil {
+		middleware.ResponseError(c, 2001, err)
+		return
+	}
+
+	tx, err := lib.GetGormPool("default")
+	if err != nil {
+		middleware.ResponseError(c, 2002, err)
+		return
+	}
+	if err := (&dao.User{}).Del(c, tx, strings.Split(removeInput.IDS, ",")); err != nil {
+		middleware.ResponseError(c, 2002, err)
+		return
+	}
+	middleware.ResponseSuccess(c, "")
+	return
+}
