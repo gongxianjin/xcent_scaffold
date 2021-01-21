fbf8e4ad773b00fd990c77ef63fcc968c7a0635f
617699485 <617699485@qq.com>
2021-01-21 15:15:26 +0800

    fixRole

--- middleware/jwt.go
+++ middleware/jwt.go
@@ -21,8 +21,7 @@ func JWTAuth() gin.HandlerFunc {
 		// 我们这里jwt鉴权取头部信息 x-token 登录时回返回token信息 这里前端需要把token存储到cookie或者本地localStorage中 不过需要跟后端协商过期时间 可以约定刷新令牌或者重新登录
 		token := c.Request.Header.Get("x-token")
 		if token == "" {
-			ResponseError(c, InternalErrorCode, errors.New("未登录或非法访问")) 
-			// response.FailWithDetailed(gin.H{"reload": true}, "未登录或非法访问", c)
+			ResponseError(c, InternalErrorCode, errors.New("未登录或非法访问"))  
 			c.Abort()
 			return
 		}
