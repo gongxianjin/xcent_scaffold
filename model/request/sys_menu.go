277103a47291894e5b4c9fa292e8ecdaa7d02e58
617699485 <617699485@qq.com>
2021-04-02 19:34:39 +0800

    addMenu

--- model/request/sys_menu.go
+++ model/request/sys_menu.go
@@ -8,16 +8,15 @@ type AddMenuAuthorityInfo struct {
 	AuthorityId string
 }
 
-
 // menu分页条件查询及排序结构体
 type SearchMenuParams struct {
 	PageInfo
-	Id  int `json:"id"`
+	Id       int    `json:"id"`
 	ParentId string `json:"parentId"`
-	Name     string   `json:"name"`
+	Name     string `json:"name"`
 }
 
-type BatchSetMenuParams struct { 
-	Ids []int `json:"ids" form:"ids"` 
-	Show     int   `json:"show"`
+type BatchSetMenuParams struct {
+	Ids  []int `json:"ids" form:"ids"`
+	Show uint  `json:"show"`
 }
